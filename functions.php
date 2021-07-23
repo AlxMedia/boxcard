5a117d141ff935b9ff9d178e52d5d5f9cf6027a2
Alexander Agnarson <alexander.agnarson@gmail.com>
2021-07-23 18:44:27 +0200

    Disable support for widgets block editor, regen lang

--- functions.php
+++ functions.php
@@ -69,6 +69,9 @@ if ( ! function_exists( 'boxcard_setup' ) ) {
 		// Enable support for selective refresh of widgets in customizer
 		add_theme_support( 'customize-selective-refresh-widgets' );
 		
+		// Disable support for widgets block editor
+		remove_theme_support( 'widgets-block-editor' );
+		
 		// Thumbnail sizes
 		add_image_size( 'boxcard-small', 200, 200, true );
 		add_image_size( 'boxcard-medium', 520, 293, true );

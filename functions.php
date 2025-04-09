180274039c38f6773e46ee8a09904d28afea87d5
Alexander Agnarson <contact@alx.media>
2025-04-09 20:20:46 +0200

    Kirki deprecated fix

--- functions.php
+++ functions.php
@@ -731,3 +731,16 @@ function boxcard_skip_link_focus_fix() {
 	<?php
 }
 add_action( 'wp_print_footer_scripts', 'boxcard_skip_link_focus_fix' );
+
+
+/*  Kirki deprecated fix
+/* ------------------------------------ */
+function boxcard_kirki_config( $config ) {
+
+	if ( isset( $config['compiler'] ) ) {
+		unset( $config['compiler'] );
+	}
+
+	return $config;
+}
+add_filter( 'kirki/config', 'boxcard_kirki_config', 999 );
